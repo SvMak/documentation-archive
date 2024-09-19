6b12192622c1807f8bf33e751cffe4fd0a44aaf4
Sviatoslav Makhynko <sviat.mak@gmail.com>
2024-09-19 18:29:41 +0200

    fix: v6.1.2

--- v6.1.2/js/services.js
+++ v6.1.2/js/services.js
@@ -60,7 +60,7 @@
         var currentModule;
         return {
             getModulesConfig: function () {
-                return $http.get('/modules/marketplace/data/modules-config.json').success(function (resp) {
+                return $http.get('/documentation-archive/v6.1.2/modules/marketplace/data/modules-config.json').success(function (resp) {
                     return resp;
                 });
             },
